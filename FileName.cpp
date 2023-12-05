257278c26e425c8fe120f3a520ad43d431527859
Анна Чернявська <146863432+atomicher@users.noreply.github.com>
2023-12-05 21:29:11 +0200

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 void find_min_max_distance(int* array, int size, int* min_value, int* max_value, int* distance) {
-    // Ініціалізування min_value і max_value першим елементом масиву
+    // Р†РЅС–С†С–Р°Р»С–Р·СѓРІР°РЅРЅСЏ min_value С– max_value РїРµСЂС€РёРј РµР»РµРјРµРЅС‚РѕРј РјР°СЃРёРІСѓ
     *min_value = *max_value = array[0];
 
-    // Iніціалізування покажчика на позиції елементів min і max в масиві
+    // IРЅС–С†С–Р°Р»С–Р·СѓРІР°РЅРЅСЏ РїРѕРєР°Р¶С‡РёРєР° РЅР° РїРѕР·РёС†С–С— РµР»РµРјРµРЅС‚С–РІ min С– max РІ РјР°СЃРёРІС–
     int* min_position = array;
     int* max_position = array;
 
-    // Знаходження мінімального і максимального елемента та їх положення
+    // Р—РЅР°С…РѕРґР¶РµРЅРЅСЏ РјС–РЅС–РјР°Р»СЊРЅРѕРіРѕ С– РјР°РєСЃРёРјР°Р»СЊРЅРѕРіРѕ РµР»РµРјРµРЅС‚Р° С‚Р° С—С… РїРѕР»РѕР¶РµРЅРЅСЏ
     for (int i = 1; i < size; i++) {
         if (array[i] < *min_value) {
             *min_value = array[i];
@@ -21,44 +21,44 @@ void find_min_max_distance(int* array, int size, int* min_value, int* max_value,
         }
     }
 
-    // Обчислення відстані між положеннями елементів
+    // РћР±С‡РёСЃР»РµРЅРЅСЏ РІС–РґСЃС‚Р°РЅС– РјС–Р¶ РїРѕР»РѕР¶РµРЅРЅСЏРјРё РµР»РµРјРµРЅС‚С–РІ
     *distance = max_position - min_position;
 }
 
 int main() {
     int size;
 
-    // Введення size в array
+    // Р’РІРµРґРµРЅРЅСЏ size РІ array
     printf("Enter the size of the array: ");
     while (scanf_s("%d", &size) != 1) {
         printf("Error. Try again: ");
-        while (getchar() != '\n');  // Очистка буфера
+        while (getchar() != '\n');  // РћС‡РёСЃС‚РєР° Р±СѓС„РµСЂР°
     }
 
-    // Виділення пам'яті в array
+    // Р’РёРґС–Р»РµРЅРЅСЏ РїР°Рј'СЏС‚С– РІ array
     int* array = (int*)malloc(size * sizeof(int));
 
-    // Введення елементів в array
+    // Р’РІРµРґРµРЅРЅСЏ РµР»РµРјРµРЅС‚С–РІ РІ array
     printf("Enter the elements of the array:\n");
     for (int i = 0; i < size; i++) {
         printf("Element %d: ", i + 1);
         while (scanf_s("%d", &array[i]) != 1) {
             printf("Error. Try again: ");
-            while (getchar() != '\n');  // Очистка буфера
+            while (getchar() != '\n');  // РћС‡РёСЃС‚РєР° Р±СѓС„РµСЂР°
         }
     }
 
     int min_value, max_value, distance;
 
-    // Отримання макс. мін. та дист.
+    // РћС‚СЂРёРјР°РЅРЅСЏ РјР°РєСЃ. РјС–РЅ. С‚Р° РґРёСЃС‚.
     find_min_max_distance(array, size, &min_value, &max_value, &distance);
 
-    // Показ рез.
+    // РџРѕРєР°Р· СЂРµР·.
     printf("\nMinimum value: %d\n", min_value);
     printf("Maximum value: %d\n", max_value);
     printf("Distance between positions: %d\n", distance);
 
-    // Вивільнення пам'яті
+    // Р’РёРІС–Р»СЊРЅРµРЅРЅСЏ РїР°Рј'СЏС‚С–
     free(array);
 
     return 0;
